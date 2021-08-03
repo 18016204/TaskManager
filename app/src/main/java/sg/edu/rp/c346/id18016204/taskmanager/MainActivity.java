7f7bc6d1256ce11f9691fb1dfb9475d6bcbd788e
18016204 <57203937+18016204@users.noreply.github.com>
2021-08-03 14:41:01 +0800

    main screen - listview

--- app/src/main/java/sg/edu/rp/c346/id18016204/taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18016204/taskmanager/MainActivity.java
@@ -2,13 +2,48 @@ package sg.edu.rp.c346.id18016204.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd;
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Task> al;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd = findViewById(R.id.btnAdd);
+        lv = findViewById(R.id.lv);
+
+        DBHelper dbh = new DBHelper(MainActivity.this);
+        al = dbh.getAllTasks();
+        dbh.close();
+
+        aa = new listAdapter(this, R.layout.row, al);
+        lv.setAdapter(aa);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  Intent i = new Intent(MainActivity.this, AddActivity.class);
+              //  startActivity(i);
+            }
+        });
+
+
+
+
+
     }
 }
