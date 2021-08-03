1d9c5b8b49a4640c26c3dbd218044b8ecb610b08
19042228 <19042228@myrp.edu.sg>
2021-08-03 16:03:43 +0800

    Notification

--- app/src/main/java/sg/edu/rp/c346/id18016204/taskmanager/AddActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18016204/taskmanager/AddActivity.java
@@ -2,14 +2,22 @@ package sg.edu.rp.c346.id18016204.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class AddActivity extends AppCompatActivity {
 
+    int reqCode = 12345;
     Button btnAdd, btnCancel;
     EditText etName, etDesc;
 
@@ -30,9 +38,29 @@ public class AddActivity extends AppCompatActivity {
                 String desc = etDesc.getText().toString();
                 DBHelper dbh = new DBHelper(AddActivity.this);
                 dbh.insertTask(name, desc);
-                Intent intent = new Intent(AddActivity.this,MainActivity.class);
-                setResult(RESULT_OK, intent);
-                finish();
+
+
+                //=================Alarm====================
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(AddActivity.this);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("name", name);
+                editor.commit();
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(AddActivity.this,
+                        ScheduledNotificationReceiver.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        AddActivity.this, reqCode,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+                //=================Alarm====================
             }
         });
 
